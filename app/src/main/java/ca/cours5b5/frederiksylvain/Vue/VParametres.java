7f8156c90e3c494f7343d81d54c02df7000a24ae
Frederik Sylvain <frederik_coolss@hotmail.com>
2018-09-11 13:29:48 -0400

    Atelier#5

--- app/src/main/java/ca/cours5b5/frederiksylvain/Vue/VParametres.java
+++ app/src/main/java/ca/cours5b5/frederiksylvain/Vue/VParametres.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import ca.cours5b5.frederiksylvain.Activites.AMenuPrincipal;
+import ca.cours5b5.frederiksylvain.Modeles.MParametres;
 import ca.cours5b5.frederiksylvain.R;
 import ca.cours5b5.frederiksylvain.Global.GConstantes;
 
@@ -34,31 +37,67 @@ public class VParametres extends Vue{
 
             super.onFinishInflate();
 
-        Spinner monSpinnerHauteur = this.findViewById(R.id.spinnerHauteur);
+        final Spinner monSpinnerHauteur = this.findViewById(R.id.spinnerHauteur);
         ArrayAdapter<Integer> adapterHauteur = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
         monSpinnerHauteur.setAdapter(adapterHauteur);
-        for(int i=GConstantes.HAUTEURMIN; i<= GConstantes.HAUTEURMAX; i++){
+        for(int i=GConstantes.HAUTEURMIN; i<= GConstantes.HAUTEURMAX; i++) {
             adapterHauteur.add(i);
         }
-        monSpinnerHauteur.setSelection(adapterHauteur.getPosition(GConstantes.HAUTEURDEF));
+        monSpinnerHauteur.setSelection(adapterHauteur.getPosition(MParametres.instance.getHauteur()));
+        monSpinnerHauteur.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Integer choix = (Integer) adapterView.getAdapter().getItem(i);
+                MParametres.instance.setHauteur(choix);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
-        Spinner monSpinnerLargeur = this.findViewById(R.id.spinnerLargeur);
+        final Spinner monSpinnerLargeur = this.findViewById(R.id.spinnerLargeur);
         ArrayAdapter<Integer> adapterLargeur = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
         monSpinnerLargeur.setAdapter(adapterLargeur);
         for(int i=GConstantes.LARGEURMIN; i<= GConstantes.LARGEURMAX; i++){
             adapterLargeur.add(i);
         }
-        monSpinnerLargeur.setSelection(adapterLargeur.getPosition(GConstantes.LARGEURDEF));
+        monSpinnerLargeur.setSelection(adapterLargeur.getPosition(MParametres.instance.getLargeur()));
+        monSpinnerLargeur.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Integer choix = (Integer) adapterView.getAdapter().getItem(i);
+                MParametres.instance.setLargeur(choix);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
-        Spinner monSpinnerGagner = this.findViewById(R.id.spinnerGagner);
+        final Spinner monSpinnerGagner = this.findViewById(R.id.spinnerGagner);
         ArrayAdapter<Integer> adapterGagner = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
         monSpinnerGagner.setAdapter(adapterGagner);
         for(int i=GConstantes.POURGAGNERMIN; i<= GConstantes.POURGAGNERMAX; i++){
             adapterGagner.add(i);
         }
-        monSpinnerGagner.setSelection(adapterGagner.getPosition(GConstantes.POURGAGNERDEF));
+        monSpinnerGagner.setSelection(adapterGagner.getPosition(MParametres.instance.getPourGagner()));
+        monSpinnerGagner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Integer choix = (Integer) adapterView.getAdapter().getItem(i);
+                MParametres.instance.setPourGagner(choix);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         Log.d("atelier04", VParametres.class.getSimpleName() + "::onFinishInflate");
     }
