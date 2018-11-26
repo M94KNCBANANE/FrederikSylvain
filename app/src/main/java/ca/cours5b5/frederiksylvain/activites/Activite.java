2701624689ca8f653e81e273aa7369dea9148261
Frederik Sylvain <frederik_coolss@hotmail.com>
2018-11-26 15:07:26 -0500

    Projet Finale : Terminé pour vrai!!!!

--- app/src/main/java/ca/cours5b5/frederiksylvain/activites/Activite.java
+++ app/src/main/java/ca/cours5b5/frederiksylvain/activites/Activite.java
@@ -3,16 +3,20 @@ package ca.cours5b5.frederiksylvain.activites;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import ca.cours5b5.frederiksylvain.controleurs.ControleurAction;
 import ca.cours5b5.frederiksylvain.controleurs.ControleurModeles;
+import ca.cours5b5.frederiksylvain.controleurs.interfaces.Fournisseur;
+import ca.cours5b5.frederiksylvain.controleurs.interfaces.ListenerFournisseur;
 import ca.cours5b5.frederiksylvain.donnees.Disque;
 import ca.cours5b5.frederiksylvain.donnees.SauvegardeTemporaire;
 import ca.cours5b5.frederiksylvain.donnees.Serveur;
 import ca.cours5b5.frederiksylvain.donnees.Transition;
+import ca.cours5b5.frederiksylvain.global.GCommande;
 import ca.cours5b5.frederiksylvain.modeles.MParametres;
 import ca.cours5b5.frederiksylvain.modeles.MPartieReseau;
 
 
-public abstract class Activite extends AppCompatActivity {
+public abstract class Activite extends AppCompatActivity implements Fournisseur{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,15 @@ public abstract class Activite extends AppCompatActivity {
 
         initialiserControleurModeles(savedInstanceState);
         initialiserApplication();
+        ControleurAction.fournirAction(this, GCommande.QUITTER, new ListenerFournisseur() {
+            @Override
+            public void executer(Object... args) {
+
+                Activite.this.finish();
+
+            }
+        });
+
 
     }
 
